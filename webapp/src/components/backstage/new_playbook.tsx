20e3ce58c8abd64358a4dc63633eec87240f73c8
Alejandro García Montoro <alejandro.garciamontoro@gmail.com>
2021-04-20 15:58:30 +0200

    Redirect to list when navigating to /playbooks/new

--- webapp/src/components/backstage/new_playbook.tsx
+++ webapp/src/components/backstage/new_playbook.tsx
@@ -1,11 +1,11 @@
 import React, {FC} from 'react';
+import {Redirect} from 'react-router-dom';
 
 import {Team} from 'mattermost-redux/types/teams';
 
 import PlaybookEdit from 'src/components/backstage/playbook_edit';
-import UpgradeModal from 'src/components/widgets/generic_modal';
-
 import {useAllowPlaybookCreationInCurrentTeam} from 'src/hooks';
+import {teamPluginUrl} from 'src/browser_routing';
 
 interface Props {
     currentTeam: Team
@@ -15,16 +15,15 @@ interface Props {
 export const NewPlaybook: FC<Props> = (props: Props) => {
     const allowPlaybookCreation = useAllowPlaybookCreationInCurrentTeam();
 
+    if (!allowPlaybookCreation) {
+        return <Redirect to={teamPluginUrl(props.currentTeam.name, '/playbooks')}/>;
+    }
+
     return (
-        <>
-            <UpgradeModal
-                show={!allowPlaybookCreation}
-            />
-            <PlaybookEdit
-                currentTeam={props.currentTeam}
-                onClose={props.onClose}
-                isNew={true}
-            />
-        </>
+        <PlaybookEdit
+            currentTeam={props.currentTeam}
+            onClose={props.onClose}
+            isNew={true}
+        />
     );
 };
